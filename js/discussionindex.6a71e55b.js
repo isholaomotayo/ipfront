8f90d366cb5e771664a25acf7778140d74a2157e
Omotayo Ishola <tayo@emergingplatforms.com>
2019-02-28 06:59:41 +0100

    first deploy

---
+++ js/discussionindex.6a71e55b.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["discussionindex"],{"0fe1":function(e,n,o){"use strict";o.r(n);var s=function(){var e=this,n=e.$createElement,o=e._self._c||n;return o("div",[o("router-view",{attrs:{joinedCourse:e.joinedCourse}})],1)},i=[],r={name:"DiscussionIndex",props:{joinedCourse:[Boolean,String]}},t=r,u=o("2877"),a=Object(u["a"])(t,s,i,!1,null,null,null);n["default"]=a.exports}}]);
+//# sourceMappingURL=discussionindex.6a71e55b.js.map
---
+++ js/logout.710333b9.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["logout"],{c100:function(t,e,o){"use strict";o.r(e);var c=function(){var t=this,e=t.$createElement,o=t._self._c||e;return o("div",[t._v(" ")])},n=[],u=o("cebc"),l=(o("cadf"),o("551c"),o("f751"),o("097d"),o("2f62")),s={name:"Logout",methods:Object(u["a"])({},Object(l["d"])(["logout"])),created:function(){this.logout(),this.$router.push("/")}},a=s,r=o("2877"),i=Object(r["a"])(a,c,n,!1,null,null,null);e["default"]=i.exports}}]);
+//# sourceMappingURL=logout.710333b9.js.map
