55e1ae93e1b6835ab2c7f861fd5ff59a1020ef0e
Omotayo Ishola <tayo@emergingplatforms.com>
2019-04-29 09:53:30 +0100

    Quiz completed update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9d243918338ff387f5f667428f371bdf.js"
+  "/precache-manifest.38d026213777ca7622cf1d91a387db26.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "npower-frontend"});
